19c738d9189f74dfb60d02753c00d14111a047ae
Cipher Lunis <cipheryxinquiries@gmail.com>
2023-12-01 19:15:20 -0600

    Initial Commit

---
+++ AmbatukamTodoList/AmbatukamTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmbatukamTodoListApp.swift
+//  AmbatukamTodoList
+//
+//  Created by Cipher Lunis on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct AmbatukamTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
